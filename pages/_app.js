87cde0e84b48f4f3cfb95bef785d374e8dfcfeb2
Carlos Castillo <closcastillo95@gmail.com>
2022-02-01 23:07:25 -0800

    start of giftee using nextjs

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,18 @@
-import '../styles/globals.css'
+import '../styles/globals.css';
+import Header from '../components/Header.js';
+import Layout from '../components/Layout.js';
+import Navbar from '../components/Navbar.js';
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <>
+      <Header></Header>
+      <Layout>
+        <Navbar></Navbar>
+        <Component {...pageProps} />
+      </Layout>
+    </>
+  )
 }
 
 export default MyApp
